03389bb0bf90079a35753c47a7f8168f15fc9ab9
Thomas Weidlich <thomas.weidlich@metrosystems.net>
2012-07-25 15:51:35 +0200

    Add some debug args

--- Java LogMon/doc/examples/run_mail.sh
+++ Java LogMon/doc/examples/run_mail.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+#
+
+
+# Enable jmx
+JMX="-Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=4711 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+# Memory dump on exit vm:
+IBMMEMDUMP="-Xdump:system+heap+java:events=vmstop"
+
+# Enable logging
+LOG="-Djava.util.logging.config.file=logging.properties"
+
+# Add $JMX,$IBMMEMDUMP or $LOG if need
 
 rm nohup.out
-
-nohup java -Djava.util.logging.config.file=logging.properties \
-	-cp logmonModMail.jar:logmon.jar:lib/mail.jar \
-	app.LogMon --config=config.xml  &
+nohup java $LOG  -cp logmonModMail.jar:logmon.jar:lib/mail.jar app.LogMon --config=config.xml  &
 
 
